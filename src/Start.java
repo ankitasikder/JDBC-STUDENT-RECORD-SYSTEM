7bf9b0b7840c02405c5ae666229237311d24f89b
ankitasikder <79798763+ankitasikder@users.noreply.github.com>
2021-06-12 18:25:47 +0530

    by ankita

--- src/Start.java
+++ src/Start.java
@@ -1,9 +1,9 @@
 import java.util.Scanner;
 import com.student.manage.*;
 /*
-CREATED BY BISWARUP BHATTACHARJEE
-EMAIL    : bbiswa471@gmail.com
-PHONE NO : 6290272740
+CREATED BY ANKITA SIKDER
+EMAIL    : ankita.sikder14@gmail.com
+PHONE NO : 8583939774
 */
 
 public class Start {
